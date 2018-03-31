c488c1c558ab1915650c050634fce6fa53150fa4
Fabio Grassiotto <fabiogr@gmail.com>
2018-03-31 10:54:21 -0300

    Changed code to use half stack strategy

--- CrackingCodeInterview/QueuesTaleTwoStacks/QueuesTaleTwoStacks/TaleTwoStacks.cpp
+++ CrackingCodeInterview/QueuesTaleTwoStacks/QueuesTaleTwoStacks/TaleTwoStacks.cpp
@@ -1,4 +1,4 @@
-/*
+/* 
 In this challenge, you must first implement a queue using two stacks. Then process q queries, where each
 query is one of the following 3 types:
 1 x: Enqueue element  into the end of the queue.
@@ -14,26 +14,56 @@ class MyQueue {
 public:
 	stack<int> stack_newest_on_top, stack_oldest_on_top;
 	void push(int x) {
+		// Push Stack - keep the newest on top,
+		// only copy over to the other stack when necessary.
 		stack_newest_on_top.push(x);
-		while (!stack_oldest_on_top.empty()) {
-			int temp = stack_oldest_on_top.top();
-			stack_oldest_on_top.pop();
-			stack_newest_on_top.push(temp);
-		}
-		while (!stack_newest_on_top.empty()) {
-			int temp = stack_newest_on_top.top();
-			stack_newest_on_top.pop();
-			stack_oldest_on_top.push(temp); 
-		}
 	}
 
 	void pop() {
-		stack_oldest_on_top.pop();
+		// To perform one pop operation, copy over the contents
+		// from the newest stack to the oldest one.
+		
+		// First check if the oldest_on_top is empty. If it is not,
+		// pop from the stack.
+		if (!stack_oldest_on_top.empty()) {
+			stack_oldest_on_top.pop();
+		}
+		else {
+			// Oldest stack is empty, so bring the data from the push stack.
+			while (!stack_newest_on_top.empty()) {
+				int temp = stack_newest_on_top.top();
+				stack_newest_on_top.pop();
+				stack_oldest_on_top.push(temp);
+			}
+			// Now pop the oldest.
+			stack_oldest_on_top.pop();
+		}
 	}
 
 	int front() {
-		return stack_oldest_on_top.top();
+		// To perform one front operation, copy over the contents
+		// from the newest stack to the oldest one.
+
+		// First check if the oldest_on_top is empty. If it is not,
+		// return from the stack.
+		int ret = 0;
+		if (!stack_oldest_on_top.empty()) {
+			ret = stack_oldest_on_top.top();
+		}
+		else {
+			// Oldest stack is empty, so bring the data from the push stack.
+			while (!stack_newest_on_top.empty()) {
+				int temp = stack_newest_on_top.top();
+				stack_newest_on_top.pop();
+				stack_oldest_on_top.push(temp);
+			}
+			// Now return the oldest.
+			ret = stack_oldest_on_top.top();
+		}
+		return ret;
 	}
+
+
 };
 
 int main() {
